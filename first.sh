7fe8f50f0a20a7b16cad093cd1196af06e31939f
Garun <garun.sngh@gmail.com>
2017-01-28 03:07:40 +0530

    hello world

--- first.sh
+++ first.sh
@@ -1,12 +1,3 @@
 #! /bin/bash
 
-echo Enter your name
-read name
-echo My name is $name
-
-if [ "$name" = "paarth" ]; then
-	echo bad boy
-else
-	echo Good boy
-fi
-echo last line
+echo Hello, World
